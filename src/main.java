f2976e7d190af4e17a8f258fc230d253f4b0f9cf
PatrickMoore1 <35699967+PatrickMoore1@users.noreply.github.com>
2020-04-13 20:17:55 -0400

    Update main.java

--- src/main.java
+++ src/main.java
@@ -9,59 +9,78 @@ public class main{
 		Vulnerability vuln2 = new Vulnerability();
 		Vulnerability vuln3 = new Vulnerability();
 		Vulnerability vuln4 = new Vulnerability();
+		Vulnerability vuln5 = new Vulnerability();
+		Vulnerability vuln6 = new Vulnerability();
+		Vulnerability vuln7 = new Vulnerability();
 
 		ArrayList<Vulnerability> PatrickTestList = new ArrayList<Vulnerability>();
 		ArrayList<Vulnerability> EliTestList = new ArrayList<Vulnerability>();
 
 		vuln1.setName("Vuln 1");
-		vuln1.setPluginId("123");
+		vuln1.setPluginId("104743");
 		vuln1.setRisk("Medium");
 
 		vuln2.setName("Vuln 2");
-		vuln2.setPluginId("456");
+		vuln2.setPluginId("41028");
 		vuln2.setRisk("High");
 
 		vuln3.setName("Vuln 3");
-		vuln3.setPluginId("789");
+		vuln3.setPluginId("58987");
 		vuln3.setRisk("Low");
 
 		vuln4.setName("Vuln 4");
-		vuln4.setPluginId("000");
+		vuln4.setPluginId("79638");
 		vuln4.setRisk("High");
+		
+		vuln5.setName("Vuln 5");
+		vuln5.setPluginId("42873");
+		vuln5.setRisk("Low");
+		
+		vuln6.setName("Vuln 6");
+		vuln6.setPluginId("108797");
+		vuln6.setRisk("Medium");
+		
+		vuln7.setName("Vuln 7");
+		vuln7.setPluginId("34460");
+		vuln7.setRisk("High");
 
 
 		PatrickTestList.add(vuln1);
 		PatrickTestList.add(vuln2);
 		PatrickTestList.add(vuln3);
 		PatrickTestList.add(vuln4);
-
-		//vuln1.setCost(20);
-		//vuln2.setCost(50);
-		//vuln3.setCost(100);
-		//vuln4.setCost(200);
-
-		EliTestList.add(vuln1);
-		EliTestList.add(vuln2);
-		EliTestList.add(vuln3);
-		EliTestList.add(vuln4);
+		PatrickTestList.add(vuln5);
+		PatrickTestList.add(vuln6);
+		PatrickTestList.add(vuln7);
 		
 		Organizer organizer = new Organizer();
 						
 		for(Vulnerability vuln : PatrickTestList) {
-			System.out.println(vuln.getName());
+			System.out.print(vuln.getName() + " | ");
+			System.out.print(vuln.getRisk() + " - ");
 			System.out.println(vuln.getCost());
+
 		}
 		
-		System.out.println("ADDING COSTS\n\n");
+		System.out.println("\nADDING COSTS\n");
 
 		organizer.addCostEstimates(PatrickTestList);
 		
 		for(Vulnerability vuln : PatrickTestList) {
-			System.out.println(vuln.getName());
+			System.out.print(vuln.getName() + " | ");
+			System.out.print(vuln.getRisk() + " - ");
 			System.out.println(vuln.getCost());
 		}
 		
-		//organizer.organizeList(EliTestList);
+		System.out.println("\nSORTING BY PRIORITY\n");
+		
+		organizer.organizeList(EliTestList);
+		
+		for(Vulnerability vuln : PatrickTestList) {
+			System.out.print(vuln.getName() + " | ");
+			System.out.print(vuln.getRisk() + " - ");
+			System.out.println(vuln.getCost());
+		}
 
 
 
